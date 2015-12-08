87968cc559433ef49c07d83033ebd1bc9a861615
Justin Edwards <justin@igrowbeards.com>
2015-12-08 16:45:49 -0600

    letter pair filter

--- Day5/Ruby/string_judger.rb
+++ Day5/Ruby/string_judger.rb
@@ -1,14 +1,20 @@
 # Judges a line seperated list (string) of words for niceness
 class StringJudger
   def judge_input(input)
-    nice_strings = 0
+    num_nice_strings = 0
     input.split("\n").each do |word|
-      nice_strings += 1 if judge_string(word) == 'nice'
+      num_nice_strings += 1 if judge_string(word) == 'nice'
     end
-    nice_strings
+    num_nice_strings
   end
 
-  private
+  def legacy_judge_input(input)
+    num_nice_strings = 0
+    input.split("\n").each do |word|
+      num_nice_strings += 1 if legacy_judge_string(word) == 'nice'
+    end
+    num_nice_strings
+  end
 
   def judge_string(str)
     if nice?(str)
@@ -18,8 +24,32 @@ class StringJudger
     end
   end
 
+  def legacy_judge_string(str)
+    if legacy_nice?(str)
+      'nice'
+    else
+      'naughty'
+    end
+  end
+
   def nice?(str)
+    letter_pair?(str)
+  end
+
+  # don't use this - it's old but necesary until refactored out
+  def legacy_nice?(str)
     repeated_letters?(str) && !blacklisted_strings?(str) && three_vowels?(str)
+  end
+
+  def letter_pair?(str)
+    match = false
+    str.split('').each_with_index do |letter, i|
+      if i + 1 < str.length
+        target = letter + str[i+1]
+        match = true if str.sub(target,'').include? target
+      end
+    end
+    match
   end
 
   def three_vowels?(str)
--- Day5/Ruby/string_judger_test.rb
+++ Day5/Ruby/string_judger_test.rb
@@ -1,10 +1,17 @@
 require 'test/unit'
 require_relative 'string_judger.rb'
+require 'pry'
 
 # test the string judger class
 class StringJudgerTest < Test::Unit::TestCase
   def test_input
-    input = "aaa\nab" # shortest nice followed by shortest naughty
-    assert_equal 1, StringJudger.new.judge_input(input)
+    sj = StringJudger.new
+    assert_equal true, sj.letter_pair?("xyxy")
+    assert_equal false, sj.letter_pair?("aaa")
+  end
+
+  def test_input_legacy
+    input = "aaa\nab" # shortest nice followed by shortest naughty - legacy
+    assert_equal 1, StringJudger.new.legacy_judge_input(input)
   end
 end
